53aa7ecf2099396e5cc61ecbaac1c26806663bbe
David Corcuera <david.corcuera@gmail.com>
2022-12-18 18:31:25 +0100

    save instance state (contador value)

--- app/src/main/java/es/davidcorcuera/activitystates/MainActivity.kt
+++ app/src/main/java/es/davidcorcuera/activitystates/MainActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import androidx.databinding.DataBindingUtil
 import es.davidcorcuera.activitystates.databinding.ActivityMainBinding
 
+const val STATE_COUNT = "state_count"
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var contador = 0
@@ -13,12 +15,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Check whether we're recreating a previously destroyed instance
+        if (savedInstanceState != null) {
+            contador = savedInstanceState.getInt(STATE_COUNT, 0)
+            Log.i("MainActivity", "Restored saved state")
+        }
+
         binding.contador = contador
         binding.button.setOnClickListener {
             contador++
             binding.contador = contador
         }
         Log.i("MainActivity", "onCreate Called")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        // save current counter value
+        outState.putInt(STATE_COUNT, contador)
+        Log.i("MainActivity","onSaveInstanceState Called")
     }
 
     override fun onStart() {
